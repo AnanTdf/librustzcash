e2dac360785a88f0471e43b0e8a59ea0f3f10138
Jack Grigg <jack@electriccoin.co>
2021-08-14 02:10:31 +0100

    sqlite-ffi: Add APIs for deriving Sapling ExtSKs and ExtFVKs

--- zcash_client_sqlite/ffi/src/lib.rs
+++ zcash_client_sqlite/ffi/src/lib.rs
@@ -1,5 +1,43 @@
-fn add(a: u32, b: u32) -> u32 {
-    a + b
+use zcash_client_backend::{
+    encoding::{
+        decode_extended_spending_key, encode_extended_full_viewing_key,
+        encode_extended_spending_key,
+    },
+    keys::spending_key,
+};
+use zcash_primitives::{
+    consensus::{Network, Parameters},
+    zip32::ExtendedFullViewingKey,
+};
+
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("Invalid encoding of a Sapling extended spending key")]
+    InvalidExtSk,
+    #[error("Key is for the wrong network")]
+    WrongNetwork,
+}
+
+fn derive_spending_keys(seed: &[u8], network: Network, accounts: u32) -> Vec<String> {
+    (0..accounts)
+        .map(|account| {
+            encode_extended_spending_key(
+                network.hrp_sapling_extended_spending_key(),
+                &spending_key(seed, network.coin_type(), account),
+            )
+        })
+        .collect()
+}
+
+fn derive_extfvk(network: Network, extsk: &str) -> Result<String, Error> {
+    match decode_extended_spending_key(network.hrp_sapling_extended_spending_key(), extsk) {
+        Ok(Some(extsk)) => Ok(encode_extended_full_viewing_key(
+            network.hrp_sapling_extended_full_viewing_key(),
+            &ExtendedFullViewingKey::from(&extsk),
+        )),
+        Ok(None) => Err(Error::WrongNetwork),
+        Err(_) => Err(Error::InvalidExtSk),
+    }
 }
 
 uniffi_macros::include_scaffolding!("zcash_client_sqlite");
--- zcash_client_sqlite/ffi/tests/bindings/test_zcash_client_sqlite.py
+++ zcash_client_sqlite/ffi/tests/bindings/test_zcash_client_sqlite.py
@@ -1,3 +1,20 @@
-from zcash_client_sqlite import add
+import unittest
 
-assert add(2, 4) == 6
+from zcash_client_sqlite import (
+    Network,
+    derive_extfvk,
+    derive_spending_keys,
+)
+
+TEST_EXTSK = 'secret-extended-key-test1qd32v4k4qqqqpqps6c09xrk35mep92r2u5vkfeq5w3wfn32q7l7g2hepqkkexkvv7hx3nntqz5nk5zuclqtdct6m90g20nr75m5q8983dq32lkkuah3qwl6zv3at9sm5d0k8myntulpk09y94j6sdmj0k2spj3v28zrdh9str993x9u6yjwne2k9s76h7zagjg6fwcwanaw2tw9ffwvj407v43jk676k39f2eslvypmj699upmd5x88n2xf83szr2wzzd0x7dadnn5c2vle3z'
+TEST_EXTFVK = 'zxviewtestsapling1qd32v4k4qqqqpqps6c09xrk35mep92r2u5vkfeq5w3wfn32q7l7g2hepqkkexkvv7hjxh8gc7nrgmy28yfktsn5lkefkeytvxcadk9pnmhuzmss8q42r9uv2j5wpm3xjnxkcjyz92mxylpfqm3tlme4sdkcgldu56xr8phd8r993x9u6yjwne2k9s76h7zagjg6fwcwanaw2tw9ffwvj407v43jk676k39f2eslvypmj699upmd5x88n2xf83szr2wzzd0x7dadnn5cce4xmm'
+
+class TestKeys(unittest.TestCase):
+    def test_derive_extsk(self):
+        self.assertEqual(derive_spending_keys(b'\0' * 32, Network.TEST_NETWORK, 1), [TEST_EXTSK])
+
+    def test_derive_extfvk(self):
+        self.assertEqual(derive_extfvk(Network.TEST_NETWORK, TEST_EXTSK), TEST_EXTFVK)
+
+if __name__ == '__main__':
+    unittest.main()
