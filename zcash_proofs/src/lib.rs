09774a4dac342f3161c51e7c7726062580776777
Cen <09ae9c@gmail.com>
2023-11-26 00:43:13 +0800

    fix wasm

--- zcash_proofs/src/lib.rs
+++ zcash_proofs/src/lib.rs
@@ -1,12 +1,18 @@
 extern crate bellman;
 extern crate blake2_rfc;
 extern crate byteorder;
-extern crate ehttp;
 extern crate ff;
 extern crate pairing;
 extern crate rand;
 extern crate sapling_crypto;
 extern crate zcash_primitives;
+extern crate gloo_net;
+
+use std::io::BufReader;
+
+use bellman::groth16::{prepare_verifying_key, Parameters, PreparedVerifyingKey, VerifyingKey};
+use gloo_net::http::Request;
+use pairing::bls12_381::Bls12;
 
 mod hashreader;
 pub mod sapling;
@@ -31,7 +37,8 @@ fn download_params_by_name(
     name: &str,
 ) -> (Parameters<Bls12>, PreparedVerifyingKey<Bls12>) {
     // https://download.z.cash/downloads/sapling-output.params
-    let resp = Request::get(format!("{}/{}", baseurl, name)).send().await.unwrap();
+    let url = format!("{}/{}", baseurl, name);
+    let resp = Request::get(&url).send().await.unwrap();
     let bytes = resp.binary().unwrap();
     let mut reader = hashreader::HashReader::new(BufReader::with_capacity(1024 * 1024, &bytes[..]));
     let params = Parameters::<Bls12>::read(&mut reader, false)
