48b2ca164c974560f33647e846d57aa76e8e78ee
Cen <09ae9c@gmail.com>
2023-11-26 11:51:32 +0800

    fix download

--- zcash_proofs/src/lib.rs
+++ zcash_proofs/src/lib.rs
@@ -6,19 +6,18 @@ extern crate pairing;
 extern crate rand;
 extern crate sapling_crypto;
 extern crate zcash_primitives;
-extern crate gloo_net;
 
 use std::io::BufReader;
 
 use bellman::groth16::{prepare_verifying_key, Parameters, PreparedVerifyingKey, VerifyingKey};
-use gloo_net::http::Request;
 use pairing::bls12_381::Bls12;
 
+mod downloadreader;
 mod hashreader;
 pub mod sapling;
 
-#[cfg(target_arch = "wasm32")]
-pub async fn download_parameters(
+// #[cfg(target_arch = "wasm32")]
+pub fn download_parameters(
     base_url: &str,
 ) -> (
     Parameters<Bls12>,
@@ -26,30 +25,33 @@ pub async fn download_parameters(
     Parameters<Bls12>,
     PreparedVerifyingKey<Bls12>,
 ) {
-    let (output_params, output_vk) = download_params_by_name(base_url, "sapling-output.params").await;
-    let (spend_params, spend_vk) = download_params_by_name(base_url, "sapling-spend.params").await;
+    let (output_params, output_vk) =
+        download_params_by_name(base_url, "sapling-output.params").unwrap();
+    let (spend_params, spend_vk) = download_params_by_name(base_url, "sapling-spend.params").unwrap();
     (spend_params, spend_vk, output_params, output_vk)
 }
 
-#[cfg(target_arch = "wasm32")]
-async fn download_params_by_name(
+fn download_params_by_name(
     baseurl: &str,
     name: &str,
-) -> (Parameters<Bls12>, PreparedVerifyingKey<Bls12>) {
+) -> Result<(Parameters<Bls12>, PreparedVerifyingKey<Bls12>), minreq::Error> {
+    use downloadreader::ResponseLazyReader;
+
     // https://download.z.cash/downloads/sapling-output.params
     let url = format!("{}/{}", baseurl, name);
-    let resp = Request::get(&url).send().await;
-    let bytes = resp.unwrap().binary().await.unwrap();
-    let mut reader = hashreader::HashReader::new(BufReader::with_capacity(1024 * 1024, &bytes[..]));
-    let params = Parameters::<Bls12>::read(&mut reader, false)
+    let req = minreq::get(url);
+    let res = ResponseLazyReader::from(req);
+
+    let downloaded = BufReader::with_capacity(1024 * 1024, res);
+    let mut hash_reader = hashreader::HashReader::new(downloaded);
+    let params = Parameters::<Bls12>::read(&mut hash_reader, false)
         .expect("couldn't deserialize Sapling spend parameters file");
     let vk = prepare_verifying_key(&params.vk);
-    println!("file hash: {}", reader.into_hash());
-    (params, vk)
+    println!("file hash: {}", hash_reader.into_hash());
+    Ok((params, vk))
 }
 
 #[test]
-#[cfg(target_arch = "wasm32")]
 fn test_download() {
     download_parameters("https://download.z.cash/downloads");
 }
